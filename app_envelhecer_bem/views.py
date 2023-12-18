edc2d2d92cfa5f0bce879d1ec14ae54ba3d43353
Allyson1998 <146742126+Allyson1998@users.noreply.github.com>
2023-12-18 10:21:38 -0300

    Add files via upload

---
+++ app_envelhecer_bem/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'usuarios/home.html')
+
+def cadastro(request):
+    return render(request, 'usuarios/cadastro.html')
+
+def login(request):
+    return render(request, 'usuarios/login.html')
+
+def menu(request):
+  return render(request, 'usuarios/menu.html')
---
+++ envelhecer_bem/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from app_envelhecer_bem import views
+
+urlpatterns = [
+    # rota, view responsavel, nome de referencia
+    # envelhecerbem.com
+    path('', views.home, name='home'),
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('login', views.login, name='login'),
+    path('menu', views.menu, name='menu'),
+]
