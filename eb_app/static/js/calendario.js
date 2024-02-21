f5923ba34445bd43e20ff3851e92b8d62ea6ec8c
Allyson1998 <146742126+Allyson1998@users.noreply.github.com>
2024-02-21 20:29:34 -0300

    Add files via upload

---
+++ eb_app/static/js/calendario.js
@@ -0,0 +1,18 @@
+const agendamento = document.querySelector(".agendamento")
+const diaAgendamento = agendamento.querySelector(".day")
+const horaAgendamento = agendamento.querySelector(".hora")
+const infoAgendamento = agendamento.querySelector(".info")
+
+const calendarioConsultas = document.querySelector("#calendario-consultas")
+
+const diasCalendario = calendarioConsultas.querySelectorAll("button")
+
+diasCalendario.forEach(dia => {
+    dia.addEventListener("click", e => {
+        e.preventDefault()
+        const dataCompleta = dia.children[0].getAttribute("datetime")
+        const data = dataCompleta.split("-");
+        diaAgendamento.innerHTML = `<p>${data[2]}/${data[1]}</p>`
+        mesAgendamento.innerHTML = `<p>${data[1]}</p>`
+    })
+})
